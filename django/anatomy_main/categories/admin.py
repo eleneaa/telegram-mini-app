3038938c5e51c116e3750078c5b17d567b7c70d5
Kulenyuk_ESt <Kulenyuk_ESt@npkvip.ru>
2024-09-13 21:14:33 +0500

    update catalog model

--- django/anatomy_main/categories/admin.py
+++ django/anatomy_main/categories/admin.py
@@ -1,3 +1,5 @@
+import copy
+
 from django.contrib import admin
 
 from categories.models import Catalog
@@ -5,6 +7,20 @@ from categories.models import Catalog
 
 @admin.register(Catalog)
 class CatalogAdmin(admin.ModelAdmin):
-    list_display = ('name', 'parent')
-    list_filter = ('parent',)
+    list_display = ['name', 'is_main', 'child_ids']
     search_fields = ('name',)
+    exclude = ['id']
+
+    is_invalid = False
+
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        if db_field.name == 'child':
+            if request.resolver_match.kwargs.get("object_id"):
+                current_instance = self.get_object(request, request.resolver_match.kwargs['object_id'])
+                if current_instance:
+                    kwargs['queryset'] = Catalog.objects.exclude(id=current_instance.id)
+
+                    if not current_instance.is_main:
+                        kwargs['queryset'] = kwargs['queryset'].exclude(is_main__in=[True])
+
+        return super().formfield_for_dbfield(db_field, request, **kwargs)
--- django/anatomy_main/categories/models.py
+++ django/anatomy_main/categories/models.py
@@ -1,11 +1,29 @@
 import uuid
-from django.contrib import admin
 from djongo import models
 
 
 class Catalog(models.Model):
-    name = models.CharField(max_length=100)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
+    is_main = models.BooleanField(verbose_name="Является главным каталогом?")
+    id = models.CharField(max_length=100, default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=100, verbose_name="Название каталога")
+    child = models.ArrayReferenceField(
+        to='self',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='childs',
+        verbose_name="ID вложенных каталогов",
+
+    )
+
+    def child_ids(self):
+        cls = type(self)
+        if self.child_id:
+            childs_array = [cls.objects.get(id=child) for child in self.child_id]
+            return childs_array
+
+    def __eq__(self, other):
+        return other == self.id
 
     def __str__(self):
         return self.name
