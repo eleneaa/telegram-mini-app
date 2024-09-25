8cce35b4008e5f6a8880710530459d2b112687d5
Филиппов Данил <Filippov_DA@npkvip.ru>
2024-09-25 14:39:04 +0500

    Add dockerfile for bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,12 +1,12 @@
 # bot.py
-
 import os
+
 from telegram import Update, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 
-# BOT_TOKEN = os.getenv('BOT_TOKEN', 'your-telegram-bot-token')
-BOT_TOKEN = '7887662113:AAH4eB61DIivFoXCYV3vivRk9-7iBDvjEKU'
-WEBAPP_URL = "https://7887-109-120-133-103.ngrok-free.app/articles/open/76767a23-8c74-4fb0-bf32-5fb3a54bbb58/"  # Ссылка на ваше мини-приложение
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7887662113:AAH4eB61DIivFoXCYV3vivRk9-7iBDvjEKU')
+WEBAPP_URL = os.getenv("WEBAPP_URL")  # Ссылка на ваше мини-приложение
+
 
 async def start(update: Update, context: CallbackContext):
     keyboard = InlineKeyboardMarkup(
@@ -15,6 +15,7 @@ async def start(update: Update, context: CallbackContext):
         ]
     )
     await update.message.reply_text("Welcome to the bot! Click the button to open the Mini App.", reply_markup=keyboard)
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
