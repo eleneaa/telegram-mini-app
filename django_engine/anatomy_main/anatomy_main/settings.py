6188073e4aa3436dd563237224bf78726e3c2bc9
NARSELON101 <kanala.23457@gmail.com>
2024-12-24 13:33:59 +0500

    fix database settings

--- django_engine/anatomy_main/anatomy_main/settings.py
+++ django_engine/anatomy_main/anatomy_main/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['salmon-ultimate-piglet.ngrok-free.app',
                  'localhost',
                  '127.0.0.1']
-
 
 # Application definition
 
@@ -78,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'anatomy_main.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -86,13 +83,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tg_bd',
-        'HOST': '192.168.10.65',
+        'HOST': '109.120.133.103',
         'PORT': 5432,
-        'USER': 'postgres',
-        "PASSWORD": 'changeme'
+        'USER': 'tg_vet_app',
+        "PASSWORD": 'v*3kdPdsa)LLsdsd'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -127,7 +123,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -142,7 +137,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 CSRF_TRUSTED_ORIGINS = [
     'https://salmon-ultimate-piglet.ngrok-free.app',  # Добавьте сюда ваш URL ngrok
