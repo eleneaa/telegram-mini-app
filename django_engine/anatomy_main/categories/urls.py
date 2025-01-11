90eeed702af413d2d0e789bd05a6881f522d3aa2
Aziz <enietogou@gmail.com>
2025-01-11 20:45:42 +0500

    change category templates

--- django_engine/anatomy_main/categories/urls.py
+++ django_engine/anatomy_main/categories/urls.py
@@ -8,7 +8,8 @@ app_name = 'categories'
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('find/', view_builder.find(Catalog, "categories_find.html"), name='find'),
+    path('find/', view_builder.find(Catalog, "all_categories.html"), name='find'),
+    path('find_preview/', view_builder.find(Catalog, "find_preview_categories_component.html", 5), name='find_preview'),
     path('favorite/<str:catalog_id>/', views.toggle_favorite, name='add_to_favorite'),
     path('open/<str:catalog_id>/', views.open_catalog, name='open_catalog'),
     path('favorite_catalogs/', views.favorite_catalogs, name='favorite_catalogs'),
